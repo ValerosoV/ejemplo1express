d55c25e04f23e79fb04d177dbc87a6d80b591a56
JosephElMejor <josephy.mendietaa@konradlorenz.edu.co>
2024-04-15 15:30:00 -0500

    Ahora encriptara, y tenemos Login
    -añadí texto en la response de authentication, ya no es solo la variable user
    -ahora requiere el uso de la librería de encriptado pal metodo sing up del js authentication
    -le agregamos un método de login donde compare si existe combinación de correo contraseña que concuerde con la información de un usuario

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const userSchema = require("../models/user");
+
+const bcrypt = require("bcrypt"); //llamado para des-encriptar
+
 router.post('/signup', async (req, res) => {
     const { usuario, correo, clave } = req.body;
     const user = new userSchema({
@@ -10,6 +13,11 @@ router.post('/signup', async (req, res) => {
     });
     user.clave = await user.encryptClave(user.clave);
     await user.save(); //save es un método de mongoose para guardar datos en MongoDB 
-    res.json(user);
+    //res.json(user);
+    res.json({
+            message : "Usuario Agregado",
+            user
+    })
+
 });
 module.exports = router;
